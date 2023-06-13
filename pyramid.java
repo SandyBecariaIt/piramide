1408c1cc599be6b1620e97f0b4a251bfbf3b8e9f
Sandy F Cortes <sandra_hakufu@hotmail.com>
2023-06-12 20:46:55 -0600

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class PyramidAsterisks {
+ 
+    public static void main(String[] args) {
+        
+      int  whiteSpace = 10;
+      int asterisksCount = 1;
+      
+      while(whiteSpace >= 0){
+        //Espacios en blanco
+        for(int i=0; i < whiteSpace ; i++){
+          System.out.print(".");
+        }
+              
+        //Asteriscos
+        for(int j=1; j <= asterisksCount ; j++){
+          System.out.print("*");
+        }
+        
+        whiteSpace--;
+        asterisksCount += 2;
+        System.out.println();
+      }
+    }
+}
